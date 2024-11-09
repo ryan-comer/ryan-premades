ec85847d532e664642c1449a1414870f62ca2cb2
ryan-comer <ryancomer94@gmail.com>
2024-11-08 20:09:45 -0600

    Unpacked santana feat

---
+++ packData/unpackData.mjs
@@ -0,0 +1,11 @@
+import {compilePack, extractPack} from '@foundryvtt/foundryvtt-cli';
+let itemPacks = [
+    'santana'
+];
+let actorPacks = []
+for (let i of itemPacks) {
+    await extractPack('packs/' + i, 'packData/' + i, {'log': true, 'documentType': 'Item', transformEntry: (entry) => {delete entry._stats; delete entry.sort; delete entry.ownership;}});
+}
+for (let i of actorPacks) {
+    await extractPack('packs/' + i, 'packData/' + i, {'log': true, 'documentType': 'Actor', transformEntry: (entry) => {delete entry._stats; delete entry.sort; delete entry.ownership;}});
+}
